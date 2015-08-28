bc708c3c6bbab8457472df79cf35300d273d233a
Tim White <fignewtonfood@gmail.com>
2015-08-28 10:01:03 -0700

    add test to Brand to for 5th spec, passed

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -19,18 +19,35 @@
         //     Store::deleteAll();
         // }
 
-        function test_save() {
+        // function test_save() {
+        //     //Arrange
+        //     $brand_name = "Nike";
+        //     $test_brand = new Brand($brand_name);
+        //
+        //     //Act
+        //     $test_brand->save();
+        //     $result = Brand::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals($test_brand, $result[0]);
+        // }
+
+        function test_getAll() {
             //Arrange
             $brand_name = "Nike";
+            $brand_name2 = "Adidas";
             $test_brand = new Brand($brand_name);
+            $test_brand->save();
+            $test_brand2 = new Brand($brand_name2);
+            $test_brand2->save();
 
             //Act
-            $test_brand->save();
             $result = Brand::getAll();
 
             //Assert
-            $this->assertEquals($test_brand, $result[0]);
+            $this->assertEquals([$test_brand, $test_brand2], $result);
         }
+
 
         // function test_getBrandName() {
         //     //Arrange
@@ -89,21 +106,6 @@
         //     $test_brand->deleteOne();
         //     //Assert
         //     $this->assertEquals([$test_brand2], Brand::getAll());
-        // }
-        // function test_getAll() {
-        //     //Arrange
-        //     $brand_name = "Work stuff";
-        //     $brand_name2 = "Home stuff";
-        //     $id = 1;
-        //     $id2 = 2;
-        //     $test_brand = new Brand($brand_name, $id);
-        //     $test_brand->save();
-        //     $test_brand2 = new Brand($brand_name2, $id2);
-        //     $test_brand2->save();
-        //     //Act
-        //     $result = Brand::getAll();
-        //     //Assert
-        //     $this->assertEquals([$test_brand, $test_brand2], $result);
         // }
         // function test_deleteAll() {
         //     //Arrange
