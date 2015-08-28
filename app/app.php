5f30eb1e5997597e353c2a3833043e58c1c0eddb
Tim White <fignewtonfood@gmail.com>
2015-08-28 11:46:46 -0700

    add brand twig and route to project

--- app/app.php
+++ app/app.php
@@ -16,14 +16,12 @@
 ));
 
   $app->get("/", function() use ($app) {
-
     return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
-
   });
 
-  // $app->get("/brands", function() use ($app) {
-  //     return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
-  // });
+  $app->get("/brands", function() use ($app) {
+      return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
+  });
   //
   // $app->get("/stores", function() use ($app) {
   //     return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
@@ -40,13 +38,13 @@
   //     return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
   // });
   //
-  // $app->post("/brands", function () use ($app) {
-  //     $id = null;
-  //     $brand = new Brand($_POST['brand_name'], $id, $_POST['due_date']);
-  //     $brand->save();
-  //     return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
-  // });
-  //
+  $app->post("/brands", function () use ($app) {
+      $id = null;
+      $brand = new Brand($_POST['brand_name'], $id, $_POST['due_date']);
+      $brand->save();
+      return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll(), 'stores' => Store::getAll()));
+  });
+
   // $app->get("/brands/{id}", function ($id) use ($app) {
   //     $brand = Brand::find($id);
   //     return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
